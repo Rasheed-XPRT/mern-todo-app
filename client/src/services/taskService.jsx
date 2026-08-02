4000bbb5c1807ac0da63927353e1e2b52c470809
Abdur Rasheed <rasheed44c@gmail.com>
2026-08-01 22:53:59 -0700

    connect frontend with production API

--- client/src/services/taskService.jsx
+++ client/src/services/taskService.jsx
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
-const API= axios.create({
-    baseURL:"http://localhost:5000/api/tasks",
-});
+// const API= axios.create({
+//     baseURL:"http://localhost:5000/api/tasks",
+// });
 
+const API= axios.create({
+        baseURL:"/api/tasks",
+    });
 export const getTasks= () => API.get("/");
 export const createTask= (task) => API.post("/",task);
 export const updateTask = (id,task)=> API.put(`/${id}`,task);
